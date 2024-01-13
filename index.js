fe99616b1b5d08199ffa05651449eec55aab2627
kz-zenoking <kpellyevanskz@gmail.com>
2024-01-13 11:27:32 +0000

    my first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const port = 3000; // ou tout autre port que vous préférez
+
+// Définir une route dynamique avec un paramètre d'ID
+app.get("/utilisateur/:id", (req, res) => {
+  const userId = req.params.id;
+  // Utilisez userId comme vous le souhaitez (par exemple, recherchez en base de données)
+  res.send(`Informations sur l'utilisateur avec l'ID : ${userId}`);
+});
+
+// Écoute du serveur
+app.listen(port, () => {
+  console.log(`Serveur en écoute sur le port ${port}`);
+});
